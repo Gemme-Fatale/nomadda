430c07ec958c0094b023359cff2594233e4a0550
Nicole <nicole.rivera061@gmail.com>
2022-08-17 16:25:41 -0700

    Navigations for page rendered in the header, and added a footer

--- app/javascript/components/components/Footer.js
+++ app/javascript/components/components/Footer.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import {  } from 'reactstrap'
 
 class Footer extends Component {
   render() {
     return (
-      <div>Footer</div>
+      <footer className='text-center text-lg-left' style={{position: "fixed", left: 0, bottom: 0, right: 0, backgroundColor: "#448899", color:"white"}}>
+        Nomadda {" "} © Gemme Fatal 2022
+      </footer>
     )
   }
 }
-
+// style={{ color: "#448899"}}
 export default Footer
