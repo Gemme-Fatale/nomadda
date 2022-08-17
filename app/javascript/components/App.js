d9d972388a2faa14cedd80a02ba8fbe2082128cb
Nicole <nicole.rivera061@gmail.com>
2022-08-17 11:44:04 -0700

    Enviroment set-up

---
+++ app/javascript/components/App.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Show from './pages/Show'
+import Index from './pages/Index'
+import ProtectedIndex from './pages/ProtectedIndex'
+import UserCreate from './pages/UserCreate'
+import UserEdit from './pages/UserEdit'
+import Resources from './pages/Resources'
+import AboutUs from './pages/AboutUs'
+import NotFound from './pages/NotFound'
+import {
+  BrowserRouter as  Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  componentDidMount(){
+    this.readNomaddaDestinations()
+  }
+
+  readNomaddaDestinations = () => {
+    fetch("/destinations")
+    .then(response => response.json())
+    .then(destinationArray => this.setState({destinations: destinationArray}))
+    .catch(errors => console.log("Destination read errors:", errors))
+  }
+
+  createUserDestinations = (mytrips) => {
+    fetch("/destinations", {
+      body: JSON.stringify(mytrips),
+      headers:{
+        "Content-Type": "application/json"
+      },
+      method: "POST"
+      
+    })
+    .then(response => response.json())
+    .then(() => this.readNomaddaDestinations())
+    .catch(errors => console.log("New destinations Errors", errors))
+  }
+
+  updateUserDestinations = (editdestinations, id) => {
+    fetch(`/destinations/${id}`, {
+      body: JSON.stringify(editdestinations),
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(destination => {
+      this.setState({
+        destinations: this.state.destinations.map(destination => {
+          if (destination.id === id) {
+            return destination = editdestinations
+          } else {
+            return destination
+          }
+        })
+      })
+    })
+    .catch(errors => console.log("Destination update errors:", errors))
+  }
+
+  deleteUserDestination = (destinationId) => {
+    fetch(`/destinations/${destinationId}`, {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "DELETE"
+    })
+    .then(response => response.json())
+    .then(() => this.readNomaddaDestinations())
+    .catch(errors => console.log ("Destination delete errors:", errors))
+  }
+
+  render() {
+    const {
+      logged_in,
+      current_user,
+      new_user_route,
+      sign_in_route,
+      sign_out_route
+    } = this.props
+    console.log("logged_in:", logged_in)
+    console.log("current_user:", current_user)
+    console.log("new_user_route:", new_user_route)
+    console.log("sign_in_route:", sign_in_route)
+    console.log("sign_out_route:", sign_out_route)
+    return(
+      <>
+        <Router>
+          <Header>This is our Header</Header>
+          <Switch>
+            <h1>Nomadda App</h1>
+          </Switch>
+          <Footer>This is our Footer</Footer>
+        </Router>
+      </>
+    )
+  }
+}
+
+export default App
---
+++ app/javascript/components/pages/UserEdit.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class UserEdit extends Component {
+  render() {
+    return (
+      <div>UserEdit</div>
+    )
+  }
+}
+
+export default UserEdit
