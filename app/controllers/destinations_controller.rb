5a069f128dec401a9b07efb6f2e8acce36fe63fe
DFWilliams <williams.dee.f99@gamil.com>
2022-08-19 09:37:23 -0700

    changes made

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -3,18 +3,4 @@ class DestinationsController < ApplicationController
         destination = Destinations.all
         render json: destination
     end
-    def create
-        destination = Destination.create(destination_params)
-
-        if destination.valid? 
-            render json: destination
-        else
-            render json: destination.errors, status: 422
-        end
-    end
-    
-    private
-    def country_params
-        params.require(:destination).permit(:location, :main_attraction, :start_date, :end_date, :travelers, :trip_details, :image, :visitable_id, :visitable_type)
-    end
 end
--- app/javascript/components/pages/Index.js
+++ app/javascript/components/pages/Index.js
@@ -4,10 +4,14 @@ class Index extends Component {
   render() {
     return (
       <>
-      <div>Browse Countries</div>
-      <div>
-        <p> Browse through the countries below and see the beautiful destinations they have to offer. </p>
-      </div>
+        <div>
+          Browse countries
+        </div>
+        <div>
+          <p> 
+            Browse through the countries below and see the beautiful destinations they have to offer.
+          </p>
+        </div>
       </>
     )
   }
