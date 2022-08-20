1ea1cfd757123779e27acffc433b34267b68ef56
Nicole <nicole.rivera061@gmail.com>
2022-08-20 13:14:56 -0700

    Home page completed with Home page tests. Footer component was touched just to remove the fixed position.

--- app/javascript/components/components/Footer.js
+++ app/javascript/components/components/Footer.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import {  } from 'reactstrap'
+import { ABSOLUTE_RESOURCE_PATH } from 'webpack/lib/ModuleFilenameHelpers'
 
 class Footer extends Component {
   render() {
     return (
-      <footer className='text-center text-lg-left' style={{position: "fixed", left: 0, bottom: 0, right: 0, backgroundColor: "#448899", color:"white"}}>
+      <footer className='text-center text-lg-left' style={{position: "relative", left: 0, bottom: 0, right: 0, backgroundColor: "#448899", color:"white"}}>
         Nomadda {" "} © Gemme Fatal 2022
       </footer>
     )
