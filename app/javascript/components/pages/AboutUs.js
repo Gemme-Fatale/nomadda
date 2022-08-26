950290a51f748daa3177a7ece87f53c14eb0a75b
Nicole <nicole.rivera061@gmail.com>
2022-08-26 15:17:22 -0700

    About us page was completed fully with testing and css. Header and Footer just added a div

--- app/javascript/components/pages/AboutUs.js
+++ app/javascript/components/pages/AboutUs.js
@@ -1,66 +1,89 @@
 import React, { Component } from "react";
-import { Grid, Row, Col, Container, Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import {
+  CardImg,
+  Collapse,
+  Button,
+  CardBody,
+  Card,
+  CardHeader,
+  CardFooter,
+} from "reactstrap";
 
 class AboutUs extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      modal: false,
-    };
-
     this.toggle = this.toggle.bind(this);
+    this.state = {
+      collapse: false,
+    };
   }
 
   toggle() {
-    this.setState({
-      modal: !this.state.modal,
-    });
+    this.setState({ collapse: !this.state.collapse });
   }
 
   render() {
     return (
       <>
-        <h2>Meet Gemme Fatale!</h2>
-        <Row sm="3">
-          <Button color="info" size="sm" onClick={this.toggle}>
-            {this.props.buttonLabel} Meet Nicole!{" "}
+        <div className="aboutuspage">
+          <h2 className="aboutusheader">We are Gemme Fatale</h2>
+          <Button className="aboutbutton" color="primary" onClick={this.toggle} style={{ marginBottom: "1rem" }}>
+            Meet Gemme Fatale
           </Button>
-          <Modal
-            isOpen={this.state.modal}
-            toggle={this.toggle}
-            className={this.props.className}
-          >
-            <ModalHeader toggle={this.toggle}>Nicole Mares Rivera</ModalHeader>
-            <ModalBody>
-              Nicole is a BAMF who loves dogs and will code you under the table.
-              Her skrills are no match so you better be on your mf A GAME! Go to{" "}
-              <a
-                href="https://www.linkedin.com/in/nicole-mares-rivera-957587245/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                {" "}
-                Nicole's LinkedIn!
-              </a>
-            </ModalBody>
-            <ModalFooter>
-              <Button color="secondary" onClick={this.toggle}>
-                Close
-              </Button>
-            </ModalFooter>
-          </Modal>
-        </Row>
-        {/* <Row>
-        <Col>
-       <a href="https://www.linkedin.com/in/pkamano/" target="_blank" rel="noreferrer">Pua LinkedIn</a>
-      </Col>
-        <Col>
-        <a href="https://www.linkedin.com/in/deanna-will-williams/" target="_blank" rel="noreferrer">Will LinkedIn</a>
-        </Col>
-        <Col><a href="https://www.linkedin.com/in/nicole-mares-rivera-957587245/" target="_blank" rel="noreferrer">Nicole LinkedIn</a></Col>
-      </Row> */}
+          <Collapse className="collapse" isOpen={this.state.collapse}>
+            <Card className="puacard">
+              <CardHeader className="aboutheader">Puana'ala K Amano</CardHeader>
+              <CardImg className="puabit" alt="Pua-bitmoji" src="https://sdk.bitmoji.com/render/panel/10211812-99378340395_2-s5-v1.png?transparent=1&palette=1&scale=2"/>
+              <CardBody className="aboutcardbody">
+                Full-Stack Developer born and raised on Maui, Hawaii the most beautiful place in the world. I enjoy the outdoors with my two dogs and chicken 'Milky'. "Continuous learning is a way of life" -Pua
+              </CardBody>
+              <CardFooter style={{textAlign: "center"}}>
+                <a
+                  href="https://www.linkedin.com/in/pkamano/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Pua's LinkedIn
+                </a>
+              </CardFooter>
+            </Card>
+            <Card className="nicolecard">
+              <CardHeader className="aboutheader">Nicole Mares Rivera</CardHeader>
+              <CardImg className="nicolebit" alt="Nicole-bitmoji" src="https://sdk.bitmoji.com/render/panel/20054902-100355118586_3-s5-v1.png?transparent=1&palette=1&scale=2"/>
+              <CardBody className="aboutcardbody">
+                Full-Stack Developer originally from New York currently living in San Diego, California with a passion for coding. An animal aficionado who loves to spend time with her family outdoors and playing sports!
+              </CardBody>
+              <CardFooter style={{textAlign: "center"}}>
+                <a
+                  href="https://www.linkedin.com/in/nicole-mares-rivera"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Nicole's LinkedIn
+                </a>
+              </CardFooter>
+            </Card>
+            <Card className="willcard">
+              <CardHeader className="aboutheader">Deanna 'Will' Williams</CardHeader>
+              <CardImg className="willbit" alt="Will-bitmoji" src="https://sdk.bitmoji.com/render/panel/10208518-100074578892_4-s5-v1.png?transparent=1&palette=1&scale=2"/>
+              <CardBody className="aboutcardbody">
+                Full-Stack Developer born and raised in a small town in Virginia but fell in love with the vibe here in San Diego, California. Dedicated to creative and meaningful digital experiences.
+              </CardBody>
+              <CardFooter style={{textAlign: "center"}}>
+                <a
+                  href="https://www.linkedin.com/in/deanna-will-williams/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Will's LinkedIn
+                </a>
+              </CardFooter>
+            </Card>
+          </Collapse>
+        </div>
       </>
     );
   }
 }
-export default AboutUs
+
+export default AboutUs;
