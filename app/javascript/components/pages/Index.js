83b8ea979141f1b2a439de967c7b87b5897c4ad0
DFWilliams <williams.dee.f99@gamil.com>
2022-08-26 10:04:18 -0700

    unprotected show is complete and added navlink to index page to route to show page and fixed the index test

--- app/javascript/components/pages/Index.js
+++ app/javascript/components/pages/Index.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react'
-import { Redirect } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { Card, CardSubtitle, Col, CardImg, CardBody, CardTitle, CardText, CardGroup } from 'reactstrap'
 
 class Index extends Component {
+
   render() {
+    
     return (
       <>
-      <div>Browse countries</div>
-      <div> Browse through the countries below and see the beautiful destinations they have to offer.</div>
+      <h2>Browse countries</h2>
+      <h4>Click on the Countries to see the beautiful destinations</h4>
         <CardGroup>
           {this.props.countries && this.props.countries.map(country => {
             return (
-              <Col sm='4' md='4' lg='4'>
-                <Card className='card'>
+              <Col sm='4' md='4' lg='4'key={country.id}>
+                <NavLink to={`/show/${country.name}`}>
+                <Card className='card' >
                   <CardImg top width='100%' src={country.image} alt='Card image cap' />
                   <CardBody>
                     <CardTitle>
                       {country.name}
                     </CardTitle>
                     <CardSubtitle>
-                      Prime location: {country.prime_location}
+                      Prime location: {country.prime_destination}
                     </CardSubtitle>
                     <CardSubtitle> 
                       NightLife Rating: {country.nightlife}
@@ -32,6 +35,7 @@ class Index extends Component {
                     </CardText>
                   </CardBody>
                 </Card>
+                </NavLink>
               </Col>)})}
         </CardGroup>
       </>
--- app/javascript/components/pages/Show.js
+++ app/javascript/components/pages/Show.js
@@ -1,11 +1,53 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import {
+  Row,
+  Card,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardText,
+  CardSubtitle,
+  Button,
+} from "reactstrap";
+import { NavLink } from "react-router-dom";
 
 class Show extends Component {
   render() {
+    const { destinations, logged_in, current_user } = this.props;
+
+    let countryDestinations = destinations.filter(
+      (destination) =>
+        destination.visitable_type === "Country" &&
+        destination.country === this.props.match.params.country
+    );
     return (
-      <div>Show</div>
-    )
+      <>
+        <div className="show=container">
+          <Row sm="3">
+            {countryDestinations &&
+              countryDestinations.map((destination) => {
+                return (
+                  <Card id="showCard" key={destination.id}>
+                    <CardImg src={destination.image} />
+                    <CardBody>
+                      <CardTitle>{destination.location}</CardTitle>
+                      <CardSubtitle>{destination.country}</CardSubtitle>
+                      <CardText>
+                        This beautiful location is known for:{" "}
+                        {destination.main_attraction}
+                      </CardText>
+                        <NavLink to={"/usercreate"}>
+                          <Button>Create A Trip!</Button>
+                        </NavLink>
+                    </CardBody>
+                  </Card>
+                );
+              })}
+          </Row>
+        </div>
+      </>
+    );
   }
 }
 
-export default Show
+export default Show;
